c91d58aac053993783cfd25352d44273f2500d1b
Maxence-3 <maxencedecarvalho3@gmail.com>
2026-01-29 06:04:25 +0100

    python_module_04

---
+++ python_module_04/ex0/ft_ancient_text.py
@@ -0,0 +1,16 @@
+def recover_ancient_text():
+    print("Accessing Storage Vault: ancient_fragment.txt")
+    try:
+        f = open("ancient_fragment.txt", "r")
+        print("Connection established...\n")
+        print("RECOVERED DATA:")
+        print(f.read())
+        f.close()
+    except FileNotFoundError:
+        print("ERROR: Storage vault not found.")
+    finally:
+        print("\nData recovery complete. Storage unit disconnected.")
+
+if __name__ == "__main__":
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    recover_ancient_text()
---
+++ python_module_04/ex1/ft_archive_creation.py
@@ -0,0 +1,26 @@
+def create_new_unit():
+    print("Initializing new storage unit: new_discovery.txt")
+
+    f = open("new_discovery.txt", "w")
+    print("Storage unit created successfully...\n")
+
+    print("Inscribing preservation data...")
+
+    entry = [
+        "[ENTRY 001] New quantum algorithm discovered",
+        "[ENTRY 002] Efficiency increased by 347%",
+        "[ENTRY 003] Archived by Data Archivist trainee"
+    ]
+
+    for entry_line in entry:
+        f.write(entry_line + "\n")
+        print(entry_line)
+
+    f.close()
+    print("\nData inscription complete. Storage unit sealed.")
+    print("Archive 'new_discovery.txt' ready for long-term preservation.")
+
+
+if __name__ == "__main__":
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
+    create_new_unit()
